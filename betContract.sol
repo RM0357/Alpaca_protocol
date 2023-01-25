b895c7a7e7466e8265ece4da88e171834644bf31
RM0357 <r.morgunoff@gmail.com>
2023-01-25 17:43:49 +0100

    initial commit

---
+++ betContract.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// Sport Betting Token with connection to Chainlink Data
+// Should be able to accept bets and payout tokens in case of victory.
